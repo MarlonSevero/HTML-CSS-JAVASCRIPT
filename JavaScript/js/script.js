b1dc8ff2fd49ebe2ada997ecc4a8eb090908319c
Marlon Severo <marlonsallessevero@hotmail.com>
2025-05-08 14:49:40 -0300

    update

---
+++ JavaScript/js/script.js
@@ -0,0 +1,17 @@
+/*
+Funcoes
+
+console.log("eu sou console");
+
+function sum(value1 = 0, value2 = 0){
+    let resultado = value1 + value2;
+    console.log(`soma: ${resultado}`);
+}
+sum(100, 100);
+sum(100,);
+
+*/ 
+
+//
+
+console.log("hello World Javascript");
